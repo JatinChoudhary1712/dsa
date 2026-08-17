154fa4d55b8e4453aaa40c78733cdb99a97f4b15
Jatin Choudhary <jatinchoudhary1712@gmail.com>
2026-08-17 16:56:01 +0000

    traverse list

---
+++ traverse/1.py
@@ -0,0 +1,11 @@
+def listTraversal(arr):
+    #code here
+    list = i 
+    for i in arr : 
+        list.append(i)def listTraversal(arr):
+                      result = []
+
+                      for i in arr:
+                          result.append(i)
+
+                      return result
