4f4500c04e9d2a6a189ceae5bcf02f0b011d34aa
Jatin <159408843+JatinChoudhary1712@users.noreply.github.com>
2026-09-01 23:52:18 +0530

    Day1 - Two pointer

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers) - 1
+
+        while i < j:
+            current = numbers[i] + numbers[j]
+
+            if current == target:
+                return [i + 1, j + 1]
+
+            elif current < target:
+                i += 1
+
+            else:
+                j -= 1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
