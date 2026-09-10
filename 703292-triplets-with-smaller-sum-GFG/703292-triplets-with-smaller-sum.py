b9ed9f8ba1397a96b0eafaae84eb6a96d38530de
Jatin <159408843+JatinChoudhary1712@users.noreply.github.com>
2026-09-10 22:39:08 +0530

    Added solution - Time: O(n^2), Space: O(1)

---
+++ 703292-triplets-with-smaller-sum-GFG/703292-triplets-with-smaller-sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countTriplets(self, sum, arr):
+        arr.sort()
+        n = len(arr)
+        count = 0 
+
+        for i in range(n-2):
+            j = i+1 
+            k = n-1 
+
+            while j < k:
+                curr_sum = arr[i] + arr[j] + arr[k]
+
+                if curr_sum < sum:
+                    count += k - j
+                    j += 1
+            
+                else:
+                    k -= 1
+
+        return count
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
